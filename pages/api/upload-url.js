566edb2da7b49cc9e191708390bbc28eabfe7da0
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-12 10:01:46 +0000

    Prettier FTW

--- pages/api/upload-url.js
+++ pages/api/upload-url.js
@@ -1,20 +1,19 @@
-import aws from 'aws-sdk';
-import { v4 as uuidv4 } from 'uuid';
+import aws from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
 
 export default async function handler(req, res) {
-
   const { file, fileType } = req.query;
 
   aws.config.update({
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
     region: process.env.REGION,
-    signatureVersion: 'v4',
+    signatureVersion: "v4",
   });
 
   const ep = new aws.Endpoint(`s3.${process.env.REGION}.wasabisys.com`);
   const s3 = new aws.S3({ endpoint: ep });
-  const fileExt = file.split('.').pop();
+  const fileExt = file.split(".").pop();
   console.log(`${uuidv4()}.${fileExt}`);
   const post = s3.createPresignedPost({
     Bucket: process.env.BUCKET_NAME,
@@ -26,7 +25,9 @@ export default async function handler(req, res) {
     Expires: 60,
     Conditions: [
       [
-        'content-length-range', 0, 8.59e9 // up to 1 GiB
+        "content-length-range",
+        0,
+        8.59e9, // up to 1 GiB
       ],
     ],
   });
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 
 export default function Upload() {
   const uploadPhoto = async (e) => {
     const file = e.target.files[0];
     const filename = encodeURIComponent(file.name);
     const fileType = encodeURIComponent(file.type);
-    const res = await fetch(`/api/upload-url?file=${filename}&fileType=${fileType}`);
+    const res = await fetch(
+      `/api/upload-url?file=${filename}&fileType=${fileType}`
+    );
     const { url, fields } = await res.json();
     const formData = new FormData();
     Object.entries({ ...fields, file }).forEach(([key, value]) => {
@@ -13,24 +15,21 @@ export default function Upload() {
     });
 
     const upload = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: formData,
     });
 
     if (upload.ok) {
-      console.log('Uploaded successfully!');
+      console.log("Uploaded successfully!");
     } else {
-      console.error('Upload failed.');
+      console.error("Upload failed.");
     }
   };
 
   return (
     <main className="flex flex-col justify-center items-center h-screen">
       <p className="text-4xl">Interclip files</p>
-      <input
-        onChange={uploadPhoto}
-        type="file"
-      />
+      <input onChange={uploadPhoto} type="file" />
     </main>
   );
 }
