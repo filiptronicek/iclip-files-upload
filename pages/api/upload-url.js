ad41677f08946815a483b2d86eaa82dda3a13110
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-12 09:49:17 +0000

    Refactor

--- pages/api/upload-url.js
+++ pages/api/upload-url.js
@@ -2,6 +2,9 @@ import aws from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function handler(req, res) {
+
+  const { file, fileType } = req.query;
+
   aws.config.update({
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
@@ -11,18 +14,20 @@ export default async function handler(req, res) {
 
   const ep = new aws.Endpoint(`s3.${process.env.REGION}.wasabisys.com`);
   const s3 = new aws.S3({ endpoint: ep });
-  const fileExt = req.query.file.split('.').pop();
+  const fileExt = file.split('.').pop();
   console.log(`${uuidv4()}.${fileExt}`);
   const post = s3.createPresignedPost({
     Bucket: process.env.BUCKET_NAME,
     Fields: {
       key: `${uuidv4()}.${fileExt}`,
-      "Content-Type": req.query.fileType
+      "Content-Type": fileType,
+      //"Content-Disposition": `attachment; filename="${file}"` // TODO: preserve filenames
     },
     Expires: 60,
     Conditions: [
-      ['content-length-range', 0, 8.59e9], // up to 1 GiB
-
+      [
+        'content-length-range', 0, 8.59e9 // up to 1 GiB
+      ],
     ],
   });
 
