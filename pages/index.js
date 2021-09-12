8e8911a0ed426c0ec594e9e7ba183a0adfafdc1b
filiptronicek <filip.tronicek@seznam.cz>
2021-09-12 08:33:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+export default function Upload() {
+  const uploadPhoto = async (e) => {
+    const file = e.target.files[0];
+    const filename = encodeURIComponent(file.name);
+    const res = await fetch(`/api/upload-url?file=${filename}`);
+    const { url, fields } = await res.json();
+    const formData = new FormData();
+
+    Object.entries({ ...fields, file }).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const upload = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (upload.ok) {
+      console.log('Uploaded successfully!');
+    } else {
+      console.error('Upload failed.');
+    }
+  };
+
+  return (
+    <>
+      <p>Upload a .png or .jpg image (max 1MB).</p>
+      <input
+        onChange={uploadPhoto}
+        type="file"
+        accept="image/png, image/jpeg"
+      />
+    </>
+  );
+}
