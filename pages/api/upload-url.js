ce720a85eab53e6a33c1d070fd840856c570a96f
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-12 09:02:36 +0000

    Wasabi things

--- pages/api/upload-url.js
+++ pages/api/upload-url.js
@@ -8,13 +8,14 @@ export default async function handler(req, res) {
     signatureVersion: 'v4',
   });
 
-  const s3 = new aws.S3();
-  const post = await s3.createPresignedPost({
+  const ep = new aws.Endpoint(`s3.${process.env.REGION}.wasabisys.com`);
+  const s3 = new aws.S3({endpoint: ep});
+  const post = s3.createPresignedPost({
     Bucket: process.env.BUCKET_NAME,
     Fields: {
       key: req.query.file,
     },
-    Expires: 60, // seconds
+    Expires: 60,
     Conditions: [
       ['content-length-range', 0, 1048576], // up to 1 MB
     ],
