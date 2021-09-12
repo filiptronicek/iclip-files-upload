d1396b715cf1c2a98fb5d58b1b1c1967201cef3c
Filip Troníček <filip.tronicek@seznam.cz>
2021-09-12 10:30:01 +0000

    Reactive drag and drop changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import "tailwindcss/tailwind.css";
+import { useState } from "react";
 
 export default function Upload() {
+  const [showOverlay, setShowOverlay] = useState(false);
+
   const uploadPhoto = async (e) => {
     const file = e?.dataTransfer?.files[0] || e.target.files[0];
     const filename = encodeURIComponent(file.name);
@@ -27,27 +30,31 @@ export default function Upload() {
   };
 
   if (typeof window !== "undefined") {
-    document.getElementById("button").onclick = () => document.getElementById("hidden-input").click();
+    if (!setShowOverlay) {
+      document.getElementById("button").onclick = () =>
+        document.getElementById("hidden-input").click();
+    }
   }
   // reset counter and append file to gallery when file is dropped
   const dropHandler = (e) => {
     e.preventDefault();
     uploadPhoto(e);
-    overlay.classList.remove("draggedover");
+    setShowOverlay(false);
   };
 
   // only react to actual files being dragged
   const dragEnterHandler = (e) => {
     e.preventDefault();
-    overlay.classList.add("draggedover");
+    setShowOverlay(true);
   };
 
   const dragLeaveHandler = (_e) => {
-    overlay.classList.remove("draggedover");
+    setShowOverlay(false);
   };
 
   const dragOverHandler = (e) => {
     e.preventDefault();
+    setShowOverlay(true);
   };
 
   return (
@@ -63,37 +70,48 @@ export default function Upload() {
             onDragLeave={dragLeaveHandler}
             onDragEnter={dragEnterHandler}
           >
-            <div
-              id="overlay"
-              className="w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md"
-            >
-              <i>
-                <svg
-                  className="fill-current w-12 h-12 mb-3 text-blue-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
-                </svg>
-              </i>
-              <p className="text-lg text-blue-700">Drop files to upload</p>
-            </div>
+            {showOverlay && (
+              <div
+                id="overlay"
+                className="w-full h-full absolute top-0 left-0 pointer-events-none z-50 flex flex-col items-center justify-center rounded-md"
+              >
+                <i>
+                  <svg
+                    className="fill-current w-12 h-12 mb-3 text-blue-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
+                  </svg>
+                </i>
+                <p className="text-lg text-blue-700">Drop your file here</p>
+              </div>
+            )}
 
             <section className="h-full overflow-auto p-8 w-full h-full flex flex-col">
               <header className="border-dashed border-2 h-full border-gray-400 py-12 flex flex-col justify-center items-center">
-                <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                  <span>Drag and drop your</span>&nbsp;
-                  <span>files anywhere or</span>
-                </p>
-                <input id="hidden-input" type="file" onChange={uploadPhoto} className="hidden" />
-                <button
-                  id="button"
-                  className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
-                >
-                  Upload a file
-                </button>
+                {!showOverlay && (
+                  <>
+                    <p className="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
+                      <span>Drag and drop your</span>&nbsp;
+                      <span>file anywhere or</span>
+                    </p>
+                    <input
+                      id="hidden-input"
+                      type="file"
+                      onChange={uploadPhoto}
+                      className="hidden"
+                    />
+                    <button
+                      id="button"
+                      className="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none"
+                    >
+                      Upload a file
+                    </button>
+                  </>
+                )}
               </header>
             </section>
           </article>
